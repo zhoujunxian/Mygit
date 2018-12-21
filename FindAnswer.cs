d51146d0685698ea6a86fcb83a72c049cffe6450
zhoulilin <43108092+zhoulilin@users.noreply.github.com>
2018-12-21 15:53:13 +0800

    Add files via upload

---
+++ FindAnswer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Experiment
+{
+    class FindAnswer
+    {
+       public static List<string> FindAnswer1(String h)
+        {
+            List<string> answer = new List<string>();
+            String p = "https://www.zhihu.com/question/([^\"]*)answer";
+            MatchCollection matches = new Regex(p).Matches(h);
+            foreach (Match match in matches)
+            {
+                answer.Add(match.ToString().Replace("/answer", "").Trim());
+                Console.WriteLine(match.ToString().Replace("/answer", "").Trim());
+            }
+            return answer;
+        }
+    }
+}
